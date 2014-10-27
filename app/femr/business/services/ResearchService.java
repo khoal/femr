91428a2640471814d3644c702c4b78e8ebe83b16
arslangondal <unfixed@liquefiedlogic.com>
2014-10-27 15:20:57 -0400

    modified research method to grab patient items with some phony data

--- app/femr/business/services/ResearchService.java
+++ app/femr/business/services/ResearchService.java
@@ -18,21 +18,84 @@
 */
 package femr.business.services;
 
+
+import com.avaje.ebean.ExpressionList;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
+import femr.business.helpers.DomainMapper;
+import femr.business.helpers.QueryHelper;
+import femr.business.helpers.QueryProvider;
+import femr.common.dto.ServiceResponse;
+import femr.data.daos.IRepository;
+import femr.data.models.*;
+import femr.common.models.PatientEncounterItem;
+import femr.common.models.PatientItem;
+import femr.common.models.VitalItem;
+import femr.util.calculations.dateUtils;
+import femr.util.stringhelpers.StringUtils;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ResearchService implements IResearchService{
+
+
+    //repositories
+    private final IRepository<IChiefComplaint> chiefComplaintRepository;
+    private final IRepository<IPatient> patientRepository;
+    private final IRepository<IPatientEncounter> patientEncounterRepository;
+    private final IRepository<IPatientEncounterVital> patientEncounterVitalRepository;
+    private final IRepository<IUser> userRepository;
+    private final IRepository<IVital> vitalRepository;
+    private final Provider<IPatientEncounterVital> patientEncounterVitalProvider;
+
+    private final DomainMapper domainMapper;
 
     /**
      * Initializes the research service and injects the dependence
      */
     @Inject
-    public ResearchService() {
-    //TODO: Inject research related repositories
+    public ResearchService (IRepository<IChiefComplaint> chiefComplaintRepository,
+                         IRepository<IPatient> patientRepository,
+                         IRepository<IPatientEncounter> patientEncounterRepository,
+                         IRepository<IPatientEncounterVital> patientEncounterVitaRepository,
+                         IRepository<IUser> userRepository,
+                         IRepository<IVital> vitalRepository,
+                         Provider<IPatientEncounterVital> patientEncounterVitalProvider,
 
+                         DomainMapper domainMapper) {
+        this.chiefComplaintRepository = chiefComplaintRepository;
+        this.patientRepository = patientRepository;
+        this.patientEncounterRepository = patientEncounterRepository;
+        this.patientEncounterVitalRepository = patientEncounterVitaRepository;
+        this.userRepository = userRepository;
+        this.vitalRepository = vitalRepository;
+        this.patientEncounterVitalProvider = patientEncounterVitalProvider;
+        this.domainMapper = domainMapper;
     }
 
     //TODO: Implement research business logic
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public ServiceResponse<List<PatientItem>> getAllPatientItems() {
+        ServiceResponse<List<PatientItem>> response = new ServiceResponse<>();
 
+        try {
+            List<? extends IPatient> patients = patientRepository.findAll(Patient.class);
+            List<PatientItem> patientItems = new ArrayList<>();
+            for (IPatient v : patients) {
+                float temp = 2;
+                patientItems.add(domainMapper.createPatientItem(v ,2,2,2, temp));
+            }
+            response.setResponseObject(patientItems);
+        } catch (Exception ex) {
+            response.addError("exception", ex.getMessage());
+        }
+        System.out.println(response.getResponseObject().toString());
+        return response;
+    }
 
 
 }
