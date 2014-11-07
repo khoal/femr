c13b2a17ef3f8f705f9271ae96dd3d17fc1bd1b1
Ken Dunlap <kdunlap4918@gmail.com>
2014-11-07 17:18:32 -0500

    Added tooltips and formatting fixes to front end graphs

---
+++ test/femr/ui/controllers/ResearchControllerTest.java
@@ -0,0 +1,10 @@
+package femr.ui.controllers;
+
+import junit.framework.TestCase;
+
+public class ResearchControllerTest extends TestCase {
+
+    public void testGetGraphPost() throws Exception {
+
+    }
+}
