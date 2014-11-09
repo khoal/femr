382783d82dd02ca1e09fb549a316eedc213268a7
Ken Dunlap <kdunlap4918@gmail.com>
2014-11-09 13:52:26 -0500

    Implemented grouped bar graph. Stacked bar graph bug fixes

--- public/js/research/research.js
+++ public/js/research/research.js
@@ -860,3 +860,18 @@ function randomInt(min, max) {
     return Math.floor(Math.random() * (1 + max - min)) + min;
 };
 //*/
+
+Array.prototype.inArray = function(comparer) {
+    for(var i=0; i < this.length; i++) {
+        if(comparer(this[i])) return true;
+    }
+    return false;
+};
+
+// adds an element to the array if it does not already exist using a comparer
+// function
+Array.prototype.pushIfNotExist = function(element, comparer) {
+    if (!this.inArray(comparer)) {
+        this.push(element);
+    }
+};
